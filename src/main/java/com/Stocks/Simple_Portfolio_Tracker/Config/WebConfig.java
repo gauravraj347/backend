f5e653b8054dc458b8bff5fb68826e69d344ae4b
gauravraj347 <127657698+gauravraj347@users.noreply.github.com>
2025-01-23 00:27:12 +0530

    Update WebConfig.java

--- src/main/java/com/Stocks/Simple_Portfolio_Tracker/Config/WebConfig.java
+++ src/main/java/com/Stocks/Simple_Portfolio_Tracker/Config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow CORS for all endpoints
-                .allowedOrigins("http://localhost:5173") // Your frontend's URL
+                .allowedOrigins("https://portfolio-frontend-rq0w.onrender.com/") // Your frontend's URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed methods
                 .allowedHeaders("*") // Allowed headers
                 .allowCredentials(true); // Allow cookies/credentials
